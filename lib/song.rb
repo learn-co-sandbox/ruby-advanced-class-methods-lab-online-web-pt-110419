b3c3fd07713a6d658eac917b0186e48c0a3e52d4
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-10 17:06:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -9,5 +9,61 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+   def self.create
+    song = self.new
+     @@all << song
+    
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.detect{|s| s.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical() 
+    @@all.sort_by { |s| s.name }
+    
+  end 
+  
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+		song_array[1] = song_array[1].chomp(".mp3")
+		song = self.new
+		song.name = song_array[1]
+		song.artist_name = song_array[0]
+		song
+  end 
+  
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = Song.create
+    song.name = result.name 
+    song.artist_name = result.artist_name
+    song
+    
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end 
+  
+  
 end
